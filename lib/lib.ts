475fa35177edb4a79b8df4b3ab6fb77868abf432
feraxhp <feraxhp+gh@gmail.com>
2026-03-18 21:22:42 -0500

    fix: exports

--- lib/lib.ts
+++ lib/lib.ts
@@ -1,10 +1,5 @@
-import { FractosState } from "./state";
-import { FractosView } from "./view";
+export { FractosState } from "./state";
+export { FractosView } from "./view";
 
 export { type Metadata, type ProjectData, type TaskData } from "./state";
-export { type FractosRenderer } from "./view";
-
-export default {
-  FractosState,
-  FractosView,
-}
+export { type FractosRenderer, type ShowState, type __Project, type __Task } from "./view";
