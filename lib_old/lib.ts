969e12705b856e8bd70047a815850f72904cbf1b
feraxhp <feraxhp+gh@gmail.com>
2026-05-03 19:22:25 -0500

    publish: Update package json

--- lib_old/lib.ts
+++
@@ -1,5 +0,0 @@
-export { FractosState } from "./state";
-export { FractosView } from "./view_old";
-
-export { type Metadata, type ProjectData, type TaskData } from "./state";
-export { type FractosRenderer, type ShowState, type __Project, type __Task } from "./view_old";
